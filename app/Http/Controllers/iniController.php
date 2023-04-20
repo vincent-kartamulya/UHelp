01bc47a4b3f78380bb6883cd7d953dab4c6e3cf9
ZakariaBerlam <129251229+ZakariaBerlam@users.noreply.github.com>
2023-04-20 20:48:08 +0700

    install dusk

--- app/Http/Controllers/iniController.php
+++ app/Http/Controllers/iniController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Facebook\WebDriver\Remote\DesiredCapabilities;
-use Facebook\WebDriver\Remote\RemoteWebDriver;
+
 
 
 class iniController extends Controller
