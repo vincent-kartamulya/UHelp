805dbd1191e565fbffe9d4b0c1cffacd22686605
ZakariaBerlam <129251229+ZakariaBerlam@users.noreply.github.com>
2023-04-20 13:47:03 +0700

    connect selenium

--- app/Http/Controllers/iniController.php
+++ app/Http/Controllers/iniController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
 
 class iniController extends Controller
 {
     public function routingawal(){
-        
+        $host = 'http://localhost:1234/wd/hub'; // Selenium server URL
+        $capabilities = DesiredCapabilities::chrome();
+        $driver = RemoteWebDriver::create($host, $capabilities);
+
+        $driver->get('https://www.example.com');
+        $title = $driver->getTitle();
+        echo $title;
+        $driver->quit();
     }
 }
