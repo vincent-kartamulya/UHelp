160c0b610ad7b8f1ab95744d5a33b9c61485377a
intanparamitha33 <intanparamitha33@gmail.com>
2023-04-21 00:07:36 +0700

    Menyelesaikan tampilan Add Event Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
+export const content = [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
     "./node_modules/flowbite/**/*.js",
-  ],
-  theme: {
+];
+export const theme = {
     extend: {
         colors: {
             'yellow-new': 'rgba(247, 192, 75, 0.9)',
@@ -18,10 +17,33 @@ module.exports = {
             'font-poppins': ['Poppins', 'sans-serif'],
         },
     },
-  },
-  plugins: [
+};
+export const plugins = [
     require('flowbite/plugin')
-  ],
-}
+];
 
+// module.exports = {
+//     content: [
+//       "./resources/**/*.blade.php",
+//       "./resources/**/*.js",
+//       "./resources/**/*.vue",
+//       "./node_modules/flowbite/**/*.js",
+//     ],
+//     theme: {
+//       extend: {
+//           colors: {
+//               'yellow-new': 'rgba(247, 192, 75, 0.9)',
+//               'yellow-hover': 'rgba(247, 192, 75, 1)',
+//               'green-new': '#539064',
+//               'dark-blue-new': '#3F487F',
+//           },
+//           fontFamily: {
+//               'font-poppins': ['Poppins', 'sans-serif'],
+//           },
+//       },
+//     },
+//     plugins: [
+//       require('flowbite/plugin')
+//     ],
+//   }
 
