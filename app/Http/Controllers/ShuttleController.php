55b6a40e62e39b85535bef8b0a1e57863808b299
ZakariaBerlam <129251229+ZakariaBerlam@users.noreply.github.com>
2023-05-24 22:05:45 +0700

    create model,database, and controller

--- app/Http/Controllers/ShuttleController.php
+++ app/Http/Controllers/ShuttleController.php
@@ -1,86 +1,108 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Shuttle;
 use App\Http\Requests\StoreShuttleRequest;
+use Illuminate\Http\Request;
 use App\Http\Requests\UpdateShuttleRequest;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+use Facebook\WebDriver\WebDriverWait;
 
 class ShuttleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function routingawal()
     {
-        //
-    }
+        $host = 'http://localhost:4444/wd/hub'; // Selenium server URL
+        $capabilities = DesiredCapabilities::chrome(); // or any other browser
+        $driver = RemoteWebDriver::create($host, $capabilities);
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        $driver->get('https://forms.gle/dKcynjUvbwggiQdx5');
+        $wait = new WebDriverWait($driver, 60);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreShuttleRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreShuttleRequest $request)
-    {
-        //
-    }
+        $inputElement = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::className('whsOnd'))
+        );
+        $inputElement->sendKeys("Alogha@gmail.com");
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Shuttle  $shuttle
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Shuttle $shuttle)
-    {
-        //
-    }
+        $nextButton = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::className('NPEfkd'))
+        );
+        $nextButton->click();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Shuttle  $shuttle
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Shuttle $shuttle)
-    {
-        //
-    }
+        $woke = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::className('AB7Lab'))
+        );
+        $woke->click();
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateShuttleRequest  $request
-     * @param  \App\Models\Shuttle  $shuttle
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateShuttleRequest $request, Shuttle $shuttle)
-    {
-        //
-    }
+        $nextButton2 = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath("//span[contains(text(),'Berikutnya')]"))
+        );
+        $nextButton2->click();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Shuttle  $shuttle
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Shuttle $shuttle)
-    {
-        //
+        $tanggalan = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::cssSelector('input[type="date"].whsOnd.zHQkBf'))
+        );
+        $tanggalan->sendKeys('10-05-2022');
+
+        $nextButton3 = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath("//span[contains(text(),'Berikutnya')]"))
+        );
+        $nextButton3->click();
+
+        $BinusianId = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath('(//input[@type="text"])[1]'))
+        );
+        $BinusianId->sendKeys("Aloha America");
+
+        $Namaku = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath('(//input[@type="text"])[2]'))
+        );
+        $Namaku->sendKeys("Kaneki ken");
+
+        $Unit = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath('(//input[@type="text"])[3]'))
+        );
+        $Unit->sendKeys("Beban Binus");
+
+        $option1 = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath('//div[@aria-label="W gabut"]'))
+        );
+        $option1->click();
+
+        $telpon = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath('(//input[@type="text"])[5]'))
+        );
+        $telpon->sendKeys("088888888");
+
+        $harinya = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath('//div[@aria-label="Senin"]'))
+        );
+        $harinya->click();
+
+        $nextButton4 = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath("//span[contains(text(),'Berikutnya')]"))
+        );
+        $nextButton4->click();
+
+        $jam1 = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath('//div[@aria-label="Jam 2"]'))
+        );
+        $jam1->click();
+
+        $turundi = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath('//div[@aria-label="Binus Alsut"]'))
+        );
+        $turundi->click();
+
+        $Finish = $wait->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::xpath("//span[contains(text(),'Kirim')]"))
+        );
+        $Finish->click();
+
+        // $driver->quit();
     }
 }
+
