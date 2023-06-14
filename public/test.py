3b8d24ababe7feeac9e7e6684a55fa8f41a51e36
ZakariaBerlam <129251229+ZakariaBerlam@users.noreply.github.com>
2023-06-14 14:25:20 +0700

    still trying Python

--- public/test.py
+++ public/test.py
@@ -1,1 +1,10 @@
-print('LMAO')
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+browser.get("https://forms.gle/p1RDYnRavCw2udRY7")
+from selenium.webdriver.common.by import By
+Chincent = browser.find_element(By.CLASS_NAME,'KHxj8b')
+Chincent.send_keys("Kontolodon")
+
