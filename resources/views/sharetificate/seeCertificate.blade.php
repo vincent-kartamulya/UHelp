91016667a36d734eba4a0907c80ea39869a21102
intanparamitha33 <intanparamitha33@gmail.com>
2023-07-16 18:15:07 +0700

    responsive mobile

--- resources/views/sharetificate/seeCertificate.blade.php
+++ resources/views/sharetificate/seeCertificate.blade.php
@@ -1,22 +1,20 @@
-@extends('master')
+@extends('masterSign')
 @section('title', 'U-Help | See Certificate')
 @section('content')
-<div id="before-blue-line" class="flex flex-col pt-[7.813vw]">
-    <div class="flex flex-row">
-        <div class="flex flex-row items-center justify-center ml-[40vw] mb-[1.25vw]">
-            <h5 class="text-center text-[1.875vw] font-bold text-dark-blue-new">Certificate Verified</h5>
-            <img class="ml-[1.042vw] items-center justify-center" src="/assets/sharetificate/verified.svg" alt="Certificate Verified">
-        </div>
+<div id="before-blue-line" class="flex flex-col pt-[3.25vw]">
+    <div class="flex flex-row items-center justify-center mb-[1.25vw]">
+        <h5 class="text-center text-[1.875vw] font-bold text-dark-blue-new">Certificate Verified</h5>
+        <img class="ml-[1.042vw] w-[4vw] items-center justify-center" src="/assets/sharetificate/verified.svg" alt="Certificate Verified">
     </div>
     <div>
         <img class="w-4/5 m-auto" src="/assets/sharetificate/divider-blue.svg" alt="Divider Blue">
     </div>
 </div>
-<div id="after-blue-line" class="flex flex-row mb-[5vw] mt-[3vw] gap-[6vw]">
-    <div id="left-content" class="justify-center items-center flex flex-col">
-        <img class="shadow-xl ml-[14.5vw] w-[35vw]" src="/assets/sharetificate/certificate.png" alt="certificate">
+<div id="after-blue-line" class="flex flex-col justify-center items-center gap-[5vw] mb-[5vw] mt-[3vw] md:flex-row sm:flex-row">
+    <div id="left-content" class="items-center flex flex-col">
+        <img class="shadow-xl w-[35vw]" src="/assets/sharetificate/certificate.png" alt="certificate">
         <a href="/seeCertificate/download">
-            <div id="download-button" class="shadow-lg w-[12vw] h-[3vw] mt-[2.5vw] ml-[14.5vw] bg-yellow-new hover:bg-yellow-hover rounded-[2vw] justify-center items-center flex flex-row">
+            <div id="download-button" class="shadow-lg w-[12vw] h-[3vw] mt-[2.5vw] bg-yellow-new hover:bg-yellow-hover rounded-[2vw] justify-center items-center flex flex-row">
                 <h5 class="font-bold text-[1.042vw] text-dark-blue-new">Download</h5>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -31,15 +29,23 @@
             </div>
         </a>
     </div>
-    <div id="right-content" class="w-[30vw] h-[28vw] bg-white border border-yellow-new shadow-shadow-custom rounded-[0.5vw] px-[2.5vw] py-[2.5vw] text-[1.3vw] text-dark-blue-new">
-        <h3 class="font-medium">This certificate was issued by</h3>
-        <h5 class="font-normal opacity-50">Bina Nusantara University</h5>
-        <h3 class="font-medium mt-[2vw]">Name of Recipient</h3>
-        <h5 class="font-normal opacity-50">Anderies</h5>
-        <h3 class="font-medium mt-[2vw]">Issue Date</h3>
-        <h5 class="font-normal opacity-50">09-04-2023</h5>
-        <h3 class="font-medium mt-[2vw]">Certificate ID</h3>
-        <h5 class="font-normal opacity-50">BN-2502040524</h5>
+    <div id="right-content" class="flex flex-col justify-center w-[30vw] h-[28vw] bg-white border border-yellow-new shadow-shadow-custom rounded-[0.5vw] px-[2.5vw] py-[2.5vw] text-[1.3vw] text-dark-blue-new">
+        <div>
+            <h3 class="font-medium">This certificate was issued by</h3>
+            <h5 class="font-normal opacity-50">Bina Nusantara University</h5>
+        </div>
+        <div>
+            <h3 class="font-medium mt-[2vw]">Name of Recipient</h3>
+            <h5 class="font-normal opacity-50">Anderies</h5>
+        </div>
+        <div>
+            <h3 class="font-medium mt-[2vw]">Issue Date</h3>
+            <h5 class="font-normal opacity-50">09-04-2023</h5>
+        </div>
+        <div>
+            <h3 class="font-medium mt-[2vw]">Certificate ID</h3>
+            <h5 class="font-normal opacity-50">BN-2502040524</h5>
+        </div>
     </div>
 </div>
 @endsection
